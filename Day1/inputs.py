058550dc16f447ac61acc84ccb1fd1675d465a48
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-01 10:01:30 +0545

    Inputs in Python

---
+++ Day1/inputs.py
@@ -0,0 +1,16 @@
+name= input("Enter your name :")
+age= input("Enter your age:")
+print("Your name is :", name)
+print("You are ",age,"years old.")
+
+value= input("Enter any value: ")
+print(type(value), value) 
+#BY DEFAULT INPUTS ARE STRING IN PYTHON. we need to type cast to the required data type 
+
+#for integer
+value = int(input("Enter a number"))
+print(type(value), value)
+
+#for float
+value = float(input("Enter a number"))
+print(type(value), value)
