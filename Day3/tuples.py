a8b8bfe175d9a6dfcc8b825ae3c3905f390a4383
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-03 21:48:34 +0545

    Tuples in python

---
+++ Day3/tuples.py
@@ -0,0 +1,28 @@
+tup=(10,20,30,40,50)
+print(type(tup))
+print(tup[0])
+print(tup[3])
+
+# #emptytuple
+emptup=()
+print(emptup)
+print(type(emptup))
+
+# #onevalue tuple
+onetup=(1,)
+onetupint=(1)
+print(type(onetup))
+print(type(onetupint))
+
+# #multivalue tuples
+age=(10,23,12,13,)
+print(age)
+marks=(45,89,98,23)
+print(marks)
+
+
+#methods in tuple
+tuple=(2,1,3,1)
+print(tuple.index(2)) #returns the index of 2 from the given tuple 
+print(tuple.count(1)) #returns the occurance of 1. 
+
