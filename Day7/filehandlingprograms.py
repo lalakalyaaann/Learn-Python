7d0c18635b1d0b3f0f49f9dff96c7e13c4f96021
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-07 23:04:15 +0545

    File Handling Programs

--- Day7/filehandlingprograms.py
+++ Day7/filehandlingprograms.py
@@ -1,25 +1,25 @@
-# #WAP to create a new file in python practice.txt and add 
-# """
-# Hi everyone 
-# We are learning File I/O
-# using Java. 
-# I like programming in Java 
-# """
+#WAP to create a new file in python practice.txt and add 
+"""
+Hi everyone 
+We are learning File I/O
+using Java. 
+I like programming in Java 
+"""
 
-# with open("practice.txt","w") as f:
+with open("practice.txt","w") as f:
 
-#     f.write("Hi everyone \n We are learning File I/O\n")
-#     f.write("using Java \n I like programming in Java.")
+    f.write("Hi everyone \n We are learning File I/O\n")
+    f.write("using Java \n I like programming in Java.")
 
 # #replace the java with python
 
-# with open("practice.txt","r") as f:
-#     data=f.read()
-# new_data=data.replace("Java","Python")
-# print(new_data)
+with open("practice.txt","r") as f:
+    data=f.read()
+new_data=data.replace("Java","Python")
+print(new_data)
 
-# with open("practice.txt","w") as f:
-#     f.write(new_data)
+with open("practice.txt","w") as f:
+    f.write(new_data)
 
 
 #search if word learning exists or not
