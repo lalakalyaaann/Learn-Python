ab239c5286d0eb2957ca9f98773ee6dec2dde1ef
kalyaaan24 <kalyandhamala24@gmail.com>
2026-07-02 21:36:22 +0545

    FOR loop and If else in django template

--- LearnDjango/project/project/views.py
+++ LearnDjango/project/project/views.py
@@ -14,8 +14,12 @@ def Homepage(request):
         'title':'Home Page ',
         'bdata':'This is body paragraph',
         'clist':['PHP','Java','Django'],
+        'number':[10,20,30,40,50,55],
         'studentdetails':[
             {'name':'Kalyan','phone':9801234567},
             {'name':'Ishan','phone':9876543210}
-        ]}
+        ],
+         'ages': 
+         [12, 17, 18, 20, 15, 25, 16, 30],
+         }
      return render(request,"index.html",data)
