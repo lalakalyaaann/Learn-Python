89de8503be49fb576de27726f5500d18e9b75e03
kalyaaan24 <kalyandhamala24@gmail.com>
2026-07-13 22:14:31 +0545

    Models in django

---
+++ LearnDjango/project/service/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from service.models import service
+
+
+class serviceadmin(admin.ModelAdmin):
+    list_display=('service_icon','service_title','service_des')
+
+admin.site.register(service,serviceadmin)
+# Register your models here.
