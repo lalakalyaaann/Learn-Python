5862ae7978a0ccba92c22ab689516496284f047a
kalyaaan24 <kalyandhamala24@gmail.com>
2026-07-26 18:51:35 +0545

    Routings and URLs of TODO app

--- TODOapp/todoproject/todoapp/views.py
+++ TODOapp/todoproject/todoapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request,"index.html")
+
+def register_view(request):
+    return render(request,"register.html")
+
+def login_view(request):
+    return render(request,"login.html")
+
+def logout_view(request):
+     return render(request,"logout.html")
+
+def delete_task(request,id):
+    return HttpResponse("Deleted")
