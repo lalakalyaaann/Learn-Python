34630f4c664031ed2d5a024597895c48e6b7fbf0
kalyaaan24 <kalyandhamala24@gmail.com>
2026-07-27 23:10:10 +0545

    Register Functionality TODO app

--- TODOapp/todoproject/todoapp/views.py
+++ TODOapp/todoproject/todoapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from .forms import RegisterForm
 
 # Create your views here.
 
@@ -6,7 +7,19 @@ def home(request):
     return render(request,"index.html")
 
 def register_view(request):
-    return render(request,"register.html")
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            print("VALID")
+            form.save()
+            return redirect("login")
+        else:
+            print(form.errors)   
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
 
 def login_view(request):
     return render(request,"login.html")
@@ -15,4 +28,4 @@ def logout_view(request):
      return render(request,"logout.html")
 
 def delete_task(request,id):
-    return HttpResponse("Deleted")
+    return HttpResponse("Deleted") 
