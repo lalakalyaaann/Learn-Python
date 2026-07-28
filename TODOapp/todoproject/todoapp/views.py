b23497ab7d2e80e6080ece59e872709c4321265d
kalyaaan24 <kalyandhamala24@gmail.com>
2026-07-28 23:29:13 +0545

    Login and logout function

--- TODOapp/todoproject/todoapp/views.py
+++ TODOapp/todoproject/todoapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,HttpResponse,redirect
 from .forms import RegisterForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def home(request):
     return render(request,"index.html")
 
@@ -22,10 +24,23 @@ def register_view(request):
     return render(request, "register.html", {"form": form})
 
 def login_view(request):
-    return render(request,"login.html")
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
+        user= authenticate(request, username=username, password=password)
+        if user:
+            login(request,user)
+            return redirect("home")
+        else:
+            return render(request,"login.html")
+
+    return render(request,"login.html")
+@login_required
 def logout_view(request):
-     return render(request,"logout.html")
+     logout(request)
+     return redirect("login")
+     
 
 def delete_task(request,id):
     return HttpResponse("Deleted") 
